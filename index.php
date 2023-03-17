3b44976818eaf4dc7411255992797faada8b0dda
Tine14 <73092575+Tine0814@users.noreply.github.com>
2023-03-17 23:30:26 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if(isset($_SESSION['users'])) header('location: Admin/dashboard.php');
 <html lang="en">
 
 <head>
-    <title>Clinic </title>
+    <title>Clinic | Landing Page</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
